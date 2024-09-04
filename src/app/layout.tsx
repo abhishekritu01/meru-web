f324fda24c2a85105b268d2b20c7789bceebd90d
abhishek <abhishekritu810219@gmail.com>
2024-09-05 00:07:16 +0530

    add toaster and auth0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <UserProvider>
+        <body className={inter.className}>
+          {children}
+          <ToastContainer />
+        </body>
+      </UserProvider>
     </html>
   );
 }
